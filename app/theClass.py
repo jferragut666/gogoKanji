f776bb277e002cbb01c17f00c48fbdb00e16ffd6
Jacob Ferragut <jacobferragut@Jacobs-MacBook-Air.local>
2017-01-20 21:12:43 -0500

    made working user class

---
+++ app/theClass.py
@@ -0,0 +1,21 @@
+import pandas as pd
+df  = pd.read_excel('/Users/jacobferragut/gogo/app/templates/gogoKanjiKanji.xlsx',0) 
+excelList = list(df.T.itertuples())
+
+
+class user():
+    def __init__(self):
+        self.deck = []
+        self.col1 = excelList[0]
+        self.col2 = excelList[1]
+        for word in xrange(len(excelList[0])):
+           self.deck.append([self.col1[word],self.col2[word], 0])
+    def getDeck(self):
+        return self.deck
+    def setDeck(self,d):
+        self.deck = d
+        
+
+
+
+
--- app/views.py
+++ app/views.py
@@ -7,11 +7,7 @@ from wtforms.validators import InputRequired, NumberRange, Length
 from wtforms import StringField, IntegerField, SelectMultipleField, \
     RadioField, TextAreaField, PasswordField, SubmitField
 import hashlib
-import pandas as pd
 
-
-df  = pd.read_excel('/Users/jacobferragut/gogo/app/templates/gogoKanjiKanji.xlsx',0) 
-print df
 Red = redis.StrictRedis()
 class LoginForm(flask_wtf.FlaskForm):
     username = StringField("Username", [InputRequired()])
@@ -40,31 +36,6 @@ def login():
         #Red.set( key, value )
 
     return render_template('login.html', form=form)
-
-@app.route('/test/', methods=['post','get'])
-def main0():
-    parsedData = 0
-    
-    form = RequestForm()
-    if form.validate_on_submit():
-        #parse sentence from the form
-        parsedData = app.parser(form.sentence.data)
-        sents = []
-        sents = parsedData.sents
-        for span in parsedData.sents:
-            sent = [parsedData[i] for i in range(span.start, span.end)]
-            break
-    lists = []
-    try:    
-        for token in sent:
-            listy=[token.orth_, token.pos_, token.dep_,token.head.orth_]
-            lists.append(listy)
-            print(listy)
-            
-        #print parsedData
-        return render_template('base.html', form=form, title = "home", data = lists)
-    except: 
-        return render_template('base.html', form = form, title = "home")    
 
 class RequestForm(flask_wtf.FlaskForm):
     sentence = wtforms.StringField('Sentence', [wtforms.validators.InputRequired()])
