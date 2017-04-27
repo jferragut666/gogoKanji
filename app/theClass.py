b2ae34ce36f60730308e74d71c530bc16c83139d
Jacob Ferragut <jacobferragut@Jacobs-MacBook-Air.local>
2017-04-26 21:58:38 -0400

    completely working, but not completely polished out

--- app/theClass.py
+++ app/theClass.py
@@ -13,67 +13,41 @@ from wtforms import StringField, IntegerField, SelectMultipleField, \
 import wtforms, flask_wtf, redis, hashlib, pickle
 
 
-# df  = pd.read_excel('/Users/jacobferragut/gogo/app/templates/gogoKanjiKanji.xlsx',0) 
-# excelList = list(df.T.itertuples())
-#eshell is the best 
 
-#example of a card: [u'\u9762\u767d\u3044', 'to help', 'datetimeHere', 0]
-#---------------------  card face --------  card back -- due date ---- number of times correct 
+#example of a card: [u'\u9762\u767d\u3044', 'to help', 'datetimeHere', 0,                          u'\u9732\u727d\u3424']
+#----------------  card face ------------- card back -- due date --- number of days til review -- furigana(hiragana above kanji) 
 class User(object):
     def __init__(self, password):
-        print "かばん"
-        print u'\u9762\u767d\u3044'
-        '''
-        self.exampleOverDueDeck = [[u"かばん", "bag", datetime(2000,4,19), -1],
-                                    [u"大抵", "usually", datetime(2017,2,4), -1],
-                                     [u"上", "above",datetime(2017,2,25), -1],
-                                    [u"犬", "dog", datetime(2017,5,25), -1]]
-        '''
-        
+
         self.deck = []#deck where you grab new cards
         R = csv.reader(open('/Users/jacobferragut/gogo/app/templates/gogoKanjiKanji.csv', 'rU'))
-        cards = [ (unicode(a, 'utf-8'), b, c) for a,b,c in R ]
+        cards = [ (unicode(a, 'utf-8'), b, c, unicode(d, 'utf-8')) for a,b,c,d in R ]
         for i in cards:
             temp = list(i)
+            temp.append("")
             temp[2] = date.today()
-            temp.append(-1)
+            temp[3] = -1
+            temp[4] = i[3]
             self.deck.append(temp)
-
+        print self.deck
         self.hashedPassword = password
-        #self.reviewDeck = [] #deck of things you got incorrect 5
-        self.overDueDeck = [] #deck of things that you probably know 43
+        self.overDueDeck = [] 
         self.overDueIndex = 0
         self.done = []
         
-        self.newCardsPerDay = 2
-        #self.dateNow = datetime.today()
+        self.newCardsPerDay = 5
         self.dateNow = date.today()
         self.tomorrow = date.today() + timedelta(days=+1) 
-        self.yesterday = date.today() + timedelta(days=-1)
         self.index = 0
-        # each card is structed as a list as shown:
-        #                                     [jin,person, (dateDue in terms of datetime.today())]
-        # for word in xrange(len(excelList[0])):
-        #     # the final zero in this list is the day its due in terms of datetime
-        #     self.deck.append([self.col1[word],self.col2[word], 0, 0])
-           
+          
     
         
-    #basically call when you start your review session
     def updateDateNow(self):
         self.dateNow = date.today()
 
     def getOverDueDeck(self):
         swapIndex = 0
         temp = 0
-        '''
-        for i in xrange(len(self.overDueDeck)):
-            if self.overDueDeck[i][2] <= self.dateNow:
-               temp = self.overDueDeck[i]
-               self.overDueDeck[i] = self.overDueDeck[swapIndex]
-               self.overDueDeck[swapIndex] = temp
-               swapIndex += 1
-        '''
         return self.overDueDeck
        
 
@@ -91,39 +65,13 @@ class User(object):
             print "damn!"
 
         
-    # def REVIEW(self, form):
-    #     #over due deck = [ "face", "back", datetime(2000, 4, 19), -1]
-    #     for i in self.overDueDeck:
-    #         print "due date: ",i[2], " date now: ", self.dateNow
-    #         if i[2] <= self.dateNow:
-    #             return i
-                #temp = raw_input("enter answer: ")
-                # if temp == i[1]:
-                #     self.supermemo(i) NOTES: review one card at a time
-                #     print "correct"   make overDueDeck[0] equal next index for card to grab
-                # else:
-                #     if i[3] != -1:
-                #         i[3] = 0
-                #     else:
-                #         i[3] = -1
-                # print "due date: ",i[2], " date now: ", self.dateNow
-                
-
-#    def updateTomorrowDate(self):
-#        if self.tomorrow <= self.dateNow: # if tomorrow is now or in the past then update tomorrow
-#            self.tomorrow = self.dateNow + timedelta(days=+1)
-#program wont work because the update to tomorrow is before appendnewcards goes off. Therefore, appendnewCards will never append new cards even though the len of done is the correct length.        
-#(it might work now because I made it so that updatetomorrowdate is after appendNewCards)
+    
     def supermemo(self, card):
-        print "inputted card[3] value: ", card[3]
         if card[3] == -1:
             card[3] = 0
-            print "card is now: ", card[3], " ", card[2]
         elif card[3]==0:
             card[2] = self.dateNow + timedelta(days=+1) 
             card[3] = 1
-            print card[2], " time delta applied"
-
         elif card[3]==1:
             card[2] = self.dateNow + timedelta(days=+7)
             card[3] = 7
@@ -138,18 +86,6 @@ class User(object):
             card[3] += card[3]*2
         return card
         
-    '''
-    supermemo 0
-
-
-            I(1)=1
-            I(2)=7
-            I(3)=16
-            I(4)=35
-            for i>4: I(i):=I(i-1)*2
-            where I(i) is the interval used after the i-th repetition.
-    '''
-
     ''' 
     One programming challenge that I had was trying to check to see if the user needed more daily cards 
     Another programming problem was making sure that a card is due on a certain day.
@@ -157,19 +93,3 @@ class User(object):
     fix cycling questions ( if get wrong it finishes)
     fix words in index cards 
     ''' 
-
-    
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from app import app
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, flash
 from wtforms.validators import InputRequired, NumberRange, Length
 
 from wtforms import StringField, IntegerField, SelectMultipleField, \
@@ -36,16 +36,11 @@ def login():
                         if len(user_object.deck) != 0:
                             user_object.overDueDeck.append(user_object.deck.pop())
                             numOfDueCards+=1
-                else:
-                    for i in xrange(len(user_object.getOverDueDeck())):
-                        if user_object.getOverDueDeck()[i][2] <= user_object.dateNow:
-                            numOfDueCards+=1
-
-                user_object.done = [False]*numOfDueCards
+                    user_object.done = [False]*numOfDueCards 
 
                 if False not in user_object.done:
                     user_object.index = 0
-                    return "done for today 0"
+                    flash("done for today at login")
                     
                 Red.set(form.username.data, pickle.dumps(user_object))
 
@@ -57,7 +52,6 @@ def login():
                 Red.set(form.username.data, pickle.dumps(User(hashlib.md5(form.password.data).hexdigest())))
     else:
         print "invalid form"
-        #Red.set( key, value )
 
     return render_template('login.html', form=form)
 
@@ -75,46 +69,36 @@ class MainForm(flask_wtf.FlaskForm):
 
 def index():
     form = MainForm()
-    #cardList = [0]
-    '''
-    [[["aa", "wowo", datetime(2014,5,1)], ["taite", "usually", datetime(2019, 6, 19)]], [[u"かばん", "bag", datetime(2000,4,19)],
-                                    [u"大抵", "usually", datetime(2017,2,4)],
-                                     [u"上", "above",datetime(2017,2,25)],
-                                    [u"犬", "dog", datetime(2017,5,25)]]]
-    '''
-
+    
     interfaceUser = pickle.loads(Red.get(session['user']))
 
+    
     interfaceUser.updateDateNow()
-    print "updatedatenow in index(): ", interfaceUser.dateNow
-    print "tomorrow in index pre updatenewcards: ", interfaceUser.tomorrow
-    #interfaceUser.appendNewCards()
     if interfaceUser.tomorrow <= interfaceUser.dateNow:
-        print "tomorrow is now or in the past"
+        print "tom<=now"
         for i in xrange(interfaceUser.newCardsPerDay):
             if len(interfaceUser.deck) != 0:
                 interfaceUser.overDueDeck.append(interfaceUser.deck.pop())
         interfaceUser.tomorrow = interfaceUser.dateNow + timedelta(days=+1)
-        print "tomorrow post update: ", interfaceUser.tomorrow
+        interfaceUser.done = []
+        for i in xrange(len(interfaceUser.overDueDeck)):
+            if interfaceUser.overDueDeck[i][2] <= interfaceUser.dateNow:
+                interfaceUser.done.append(False)
+            else:
+                interfaceUser.done.append(True)
 
-        Red.set(session['user'], pickle.dumps(interfaceUser))
-        return render_template('login.html', form=LoginForm())
-    else:
-        print "damn!"
-    #interfaceUser.updateTomorrowDate() 
     index = interfaceUser.index 
     if form.validate_on_submit():
+        print "val on sub"
         if form.answerBox.data == interfaceUser.overDueDeck[index][1]:
             print "correct"
-
             if interfaceUser.overDueDeck[index][3] != -1:
                 interfaceUser.done[index] = True
                 print interfaceUser.overDueDeck[index][3], " not equal to -1" 
             print "interfaceUser.overDueDeck[",index,"] has been updated"
             interfaceUser.overDueDeck[index] = interfaceUser.supermemo(interfaceUser.overDueDeck[index])
-
+            print interfaceUser.overDueDeck
             if index + 1 < len(interfaceUser.overDueDeck):
-                #print index + 1, " < len(interfaceUser.overDueDeck)"
                 for i in xrange(index+1,len(interfaceUser.overDueDeck)):
                     if interfaceUser.done[i] == False:
                         interfaceUser.index = i
@@ -126,22 +110,21 @@ def index():
                             print "false in done and index = first false"
                             interfaceUser.index = interfaceUser.done.index(False)
                         else:
-                            return "done"
+                            flash("done")
 
             elif False in interfaceUser.done: 
                 interfaceUser.index = interfaceUser.done.index(False)
-                print "index + 1 >= len(interfaceUser.overdueDeck) and index = first false at ", interfaceUser.index
-            else:
-                #flash("done for today")
-                interfaceUser.index = 0
-                return "done for today"
-
-        print interfaceUser.done
-        print interfaceUser.overDueDeck
+        else:
+            message = 'incorrect, the correct answer was: ' + interfaceUser.overDueDeck[index][1]
+            flash(message)
+            interfaceUser.overDueDeck[index][3] = -1
+            interfaceUser.overDueDeck[index][2] = date.today() 
     else:
         print "invalid form"
 
+    if False not in interfaceUser.done: 
+        interfaceUser.index = 0
+        flash("done for today 0")
     Red.set(session['user'], pickle.dumps(interfaceUser))
-
-    return render_template("interface.html", card=interfaceUser.overDueDeck[interfaceUser.index][0], form=form)
+    return render_template("interface.html", card=interfaceUser.overDueDeck[interfaceUser.index][0], furigana = interfaceUser.overDueDeck[interfaceUser.index][4], form=form)
 
